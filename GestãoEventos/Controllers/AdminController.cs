d98618182255a7dcf94c8b313a000134f03d2db6
Hyper <128801212+FKSHyper@users.noreply.github.com>
2026-05-08 22:47:17 +0100

    Controlador Admin

---
+++ GestãoEventos/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestãoEventos.Controllers
+{
+    // Controlador para funcionalidades administrativas
+    [Authorize(Roles = "Admin")] // Apenas usuários com a função "Admin" podem acessar
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
