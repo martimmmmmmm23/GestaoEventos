b8f47ea4efb488a2ca8324098471a8a3a506d63b
Hyper <128801212+FKSHyper@users.noreply.github.com>
2026-05-08 20:03:32 +0100

    DbContext modificado e com DbSet feitos Program.cs alterado para o Login

--- GestãoEventos/Data/GestaoEventosDbContext.cs
+++ GestãoEventos/Data/GestaoEventosDbContext.cs
@@ -1,13 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using GestãoEventos.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using GestãoEventos.Data.Classes;
 
 namespace GestãoEventos.Data
 {
-    public class GestaoEventosDbContext : DbContext
+    public class GestaoEventosDbContext : IdentityDbContext<ApplicationUser>
     {
         public GestaoEventosDbContext(DbContextOptions<GestaoEventosDbContext> options)
      : base(options)
         {
         }
+        DbSet<Evento> Eventos { get; set; }
+
+        //DbSet<Inscricao> Inscricoes { get; set; }
+
+        DbSet<Participante> Participantes { get; set; }
     }
 }
